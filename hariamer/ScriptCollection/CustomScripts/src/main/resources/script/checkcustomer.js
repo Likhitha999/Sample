31d4e04b028d6b7e18d98576e1ef35484a1542ba
root <root@ip-172-31-69-163.ec2.internal>
2025-01-15 13:22:22 +0000

    commit from environment

--- hariamer/ScriptCollection/CustomScripts/src/main/resources/script/checkcustomer.js
+++ hariamer/ScriptCollection/CustomScripts/src/main/resources/script/checkcustomer.js
@@ -11,8 +11,8 @@ function processData(message) {
     var body = message.getBody();
     /*To set the body, you can use the following method. Refer SCRIPT APIs document for more detail*/
     //message.setBody(body + " modified from js");
-    //Headers
-    //modified in git
+    //Headers 
+    //modified in IS
     var headers = message.getHeaders();
     var value = headers.get("oldHeader");
     message.setHeader("oldHeader", value + " modified");
