ff9c7889e34b9d4a0f2bcb91a039442f769e12a0
Likhitha999 <59717835+Likhitha999@users.noreply.github.com>
2025-01-22 14:22:50 +0530

    Update checkcustomer.js

--- hariamer/ScriptCollection/CustomScripts/src/main/resources/script/checkcustomer.js
+++ hariamer/ScriptCollection/CustomScripts/src/main/resources/script/checkcustomer.js
@@ -12,7 +12,7 @@ function processData(message) {
     /*To set the body, you can use the following method. Refer SCRIPT APIs document for more detail*/
     //message.setBody(body + " modified from js");
     //Headers 
-    //modified in IS
+    //Changed in GIT Repository
     var headers = message.getHeaders();
     var value = headers.get("oldHeader");
     message.setHeader("oldHeader", value + " modified");
